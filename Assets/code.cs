01d1a41d77e3149eae0120d6f98331981c80488c
Rayson-LD <72185590+Rayson-LD@users.noreply.github.com>
2021-04-28 17:10:00 +0530

    initial commit

---
+++ Assets/code.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class code : MonoBehaviour 
+{
+	public float  growscale = 1.2f;
+	public float  max = 14;
+	// Use this for initialization
+	void   Start() 
+	{
+		if (growscale < 1)
+		{
+			print("The ball is too small");
+
+		}
+
+	}
+	
+	void OnMouseDown()
+	{
+		transform.localScale *= growscale;
+		if(transform.localScale.x >= max) 
+		{
+			Destroy(gameObject) ;
+		}
+	
+
+	}
+}
